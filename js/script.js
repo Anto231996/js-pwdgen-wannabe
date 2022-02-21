d98bf9ded75e974569edb39605d685735e4371a5
Anto231996 <tony2396@hotmail.it>
2022-02-21 16:49:02 +0100

    Aggiunto console.log

--- js/script.js
+++ js/script.js
@@ -1,11 +1,15 @@
 const firstName = prompt("What's your first name?");
 document.getElementById("first-name").innerHTML = firstName;
+console.log(firstName);
 
 const lastName = prompt("What's your last name?");
 document.getElementById("last-name").innerHTML = lastName;
+console.log(lastName);
 
 const favoriteColor = prompt("What's your favorite color?");
 document.getElementById("favorite-color").innerHTML = favoriteColor;
+console.log(favoriteColor);
 
 const password = document.getElementById("password");
 password.innerHTML = `${firstName + lastName + favoriteColor}`;
+console.log(firstName + lastName + favoriteColor);
