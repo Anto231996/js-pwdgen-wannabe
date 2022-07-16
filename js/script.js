c61bd838adcec7ae537c570cd15e47a276e12a84
Anto231996 <tony2396@hotmail.it>
2022-07-16 16:52:05 +0200

    fix code

--- js/script.js
+++ js/script.js
@@ -10,6 +10,6 @@ const favoriteColor = prompt("What's your favorite color?");
 document.getElementById("favorite-color").innerHTML = favoriteColor;
 console.log(favoriteColor);
 
-const password = document.getElementById("password");
+let password = document.getElementById("password");
 password.innerHTML = `${firstName}${lastName}${favoriteColor}`;
 console.log(`${firstName}${lastName}${favoriteColor}`);
